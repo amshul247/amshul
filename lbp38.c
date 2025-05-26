421c9cf52ee9e49832d20a11a4664d8c52c7d11b
amshul247 <amshulmamidipally91@gmail.com>
2025-05-26 11:20:55 +0530

    Add files via upload

---
+++ lbp38.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+int main()
+{
+	int n;
+	printf("enter a number:",n);
+	scanf("%d",&n);
+	if(n%2!=0)
+	{
+		printf("weird");
+	}
+	 else if(n%2==0,n>=6,n<=20)
+	 {
+	 	printf("not weird");
+	 }
+	 else if(n%2==0,n>=6,n<=20)
+	 {
+	 	printf("weird");
+	 }
+	 else 
+	 {
+	 	printf("not weird");
+	 	
+	 }
+}
---
+++ lbp39.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+#include<math.h>
+int main()
+{
+	int a,m,n,p;
+	scanf("%d%d%d",&a,&m,&n);
+	p=pow(a,m*n);
+	printf("%d",p);
+	return 0;
+}
